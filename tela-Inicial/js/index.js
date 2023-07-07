1d75283595020294e6bd84d162a5dca446c690c1
maycon.martinelli <maycongomesmartinelli@gmail.com>
2023-07-07 16:28:43 -0400

    1

---
+++ tela-Inicial/js/index.js
@@ -0,0 +1,20 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../img/close_white_36dp.svg";
+    }
+}
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+
+  }
+  
